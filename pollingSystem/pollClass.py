77dd81a09c05104f1aaedf6180663afdbadf26c2
Marino Echavarria <marinoechavarria@gmail.com>
2020-05-07 16:36:22 -0400

    Polling class implemented

---
+++ pollingSystem/pollClass.py
@@ -0,0 +1,36 @@
+class Poll:
+    def __init__(self, group, pollType, unanimous):
+        self.pollType = pollType
+        self.unanimous = unanimous
+
+        self.poll = {}
+        for x in group:
+            self.poll[x] = None
+
+    def vote(user, userVote):
+        if self.poll.has_key(user) and self.poll[user] is None:
+            self.poll[user] = userVote
+
+    def checkPoll():
+        if self.unanimous:
+            result = True
+            for answer in self.poll.values():
+                result = result and answer
+            return result
+        else:
+            #initializa an empty dictionary to count results
+            resultDict = {}
+            for answer in self.poll.values():
+                resultDict[answer] = resultDict.get(answer,0) + 1
+            #counts the results
+            maxVal = max(resultDict.values())
+            #extracts the maximum value
+            tieList = []
+            for user in self.poll.keys():
+                if self.poll[user] == maxVal:
+                    tieList.append(user)
+            #extracts all
+            if len(tieList) == 1:
+                return True, tieList
+            else:
+                return False, tieList
