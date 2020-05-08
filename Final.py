8b34ac2f0decae468af7380b31b967bd5459c847
taqace <39810855+taqace@users.noreply.github.com>
2020-05-07 23:39:36 -0400

    added group page

--- Final.py
+++ Final.py
@@ -2,7 +2,6 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from kivy.uix.popup import Popup
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
@@ -13,14 +12,14 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
-from Projects import SelectableButton,SelectableRecycleGridLayout
+from Projects import SelectableRecycleGridLayout
 from kivy.uix.recycleview import RecycleView
 from kivy.properties import ObjectProperty, StringProperty,BooleanProperty,ListProperty
 from kivy.uix.popup import Popup
 from kivy.clock import Clock
 
 
-cred = credentials.Certificate("CS322-FinalProject\serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 eff = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -30,6 +29,8 @@ Window.size = (1024,768)
 class screen1(Screen):
     pass
 class screen2(Screen):
+    pass
+class GroupPage(Screen):
     pass
 class Projects(Screen):
     
@@ -42,17 +43,11 @@ class Projects(Screen):
     def on_enter(self):
         self.getProjects()
 
-    def newProject(self):
-        self.Project = newEntry()
-        print('popup')
-        self.Project.open()
-
     def getProjects(self):
         self.data_projects.clear()
         docs = db.collection(u'Project').stream()
         for doc in docs:
             temp = doc.to_dict()
-            print(temp)
             self.data_projects.append(temp['name'])
 
 class MyLayout(FloatLayout):
@@ -84,7 +79,11 @@ class newEntry(Screen):
     def clocked_switch(self):
         Clock.schedule_once(self.switch_screenback,.0)
 
+class SelectableButton(Button):
 
+    def on_release(self):
+        app = App.get_running_app()
+        app.root.scr_mngr.current = "GroupPage"
 
 class MyApp(App):
     
