23758065e66530c87054db03517398c988351298
Marino Echavarria <marinoechavarria@gmail.com>
2020-05-08 17:00:12 -0400

    Added whitelist and blacklist logic to the groupInvite.py file

--- groupManager/groupInvite.py
+++ groupManager/groupInvite.py
@@ -5,8 +5,10 @@ import string
 print(sys.path)
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'helperFunctions'))
-
+sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
 import emailsender
+import databaseAccessors
+
 
 import firebase_admin
 from firebase_admin import credentials
@@ -17,21 +19,28 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def inviteUser(sender, receiver, groupName):
-    #### Initializes random string as invite passcode
-    letters = string.ascii_lowercase #gets set of lowercase ascii characters
-    randStr = ""
+    userDocument = databaseAccessors.getUserDocument(reciever)
+    if sender in userDocument['whitelist']:
+        groupProjectDoc = db.collection(u'Project).document(groupName.encode("utf-8")).get().to_dict()
+        newMemberList = groupProjectDoc["members"].append(user)
+        groupProjectDoc.update({u'members' : newMemberList})
+        emailsender.sendMail(userDocument['email'],"Invite to " + groupName, "Hello you have recieved an invite to " + groupName + ". This was a whitelisted user so you have immediete access")
+        userDocument.update({ (groupName+"InviteCode").encode("utf-8") : randStr})
 
-    for i in range(8):
-        randStr = randStr + random.choice(letters) #iterativly append random ascii chars to a string
-    #####
-    userRef = db.collection(u'Project').document(u'pAKCeGKWGqm1B2MmLMuT').collection(u'Users')
-    userDocument = userRef.document(receiver.encode("utf-8")).get().to_dict()
-    emailsender.sendMail(userDocument['email'],"Invite to " + groupName, "Hello you have recieved an invite to " + groupName +". Enter the following code to enter: " + randStr)
-    userDocument.update({ (groupName+"InviteCode").encode("utf-8") : randStr})
+    #### Initializes random string as invite passcode
+    elif not sender in userDocument['blacklist']:
+        letters = string.ascii_lowercase #gets set of lowercase ascii characters
+        randStr = ""
+        for i in range(8):
+            randStr = randStr + random.choice(letters) #iterativly append random ascii chars to a string
+        #####
+        emailsender.sendMail(userDocument['email'],"Invite to " + groupName, "Hello you have recieved an invite to " + groupName +". Enter the following code to enter: " + randStr)
+        userDocument.update({ (groupName+"InviteCode").encode("utf-8") : randStr})
+    else:
+        pass
 
 def acceptInvite(user, groupName, inviteCode):
-    userRef = db.collection(u'Project').document(u'pAKCeGKWGqm1B2MmLMuT').collection(u'Users')
-    userDocument = userRef.document(user.encode("utf-8")).get().to_dict()
+    userDocument = databaseAccessors.getUserDocument(user)
     if inviteCode == userDocument[groupName+"InviteCode"]:
         groupProjectDoc = db.collection(u'Project).document(groupName.encode("utf-8")).get().to_dict()
         newMemberList = groupProjectDoc["members"].append(user)
@@ -39,3 +48,15 @@ def acceptInvite(user, groupName, inviteCode):
         return True
     else:
         return False
+
+def appendToListAttrib(user,attrib,value):
+    userDocument = databaseAccessors.getUserDocument(user)
+    newAttribList = databaseAccessors[attrib].append(value)
+    userDocument.update({attrib.encode("utf-8") : newAttribList})
+
+def addWhiteList(user,userToAdd):
+    appendToListAttrib(user,"whitelist",userToAdd)
+
+
+def addBlackList(user,userToAdd):
+    appendToListAttrib(user,"blacklist",userToAdd)
